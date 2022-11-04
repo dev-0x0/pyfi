ed513a302c0b3cb98bbd58baff02030cdf7cc5d6
dev-0x0 <brianmacdermott@protonmail.com>
2022-11-04 19:38:33 -0400

    Resolving curses bugs

--- blackout.py
+++ blackout.py
@@ -20,8 +20,8 @@ import logging
 import argparse
 from time import sleep
 from turtle import color
-from utils import *
-from subprocess import Popen
+from utils import Utils
+from subprocess import Popen, PIPE
 from threading import Thread
 #from multiprocessing import Process, Manager
 from scapy.all import *
@@ -37,18 +37,19 @@ BROADCAST_ADDR = "FF:FF:FF:FF:FF:FF"
 
 class Blackout:
 
-    def __init__(self, interface, stdscr, sniffer_window, control_window):
+    def __init__(self, interface, stdscr, window):
+
+        self.utils = Utils(interface, stdscr, window)
 
         # Set curses screen object
         self.stdscr = stdscr
-        self.sniffer_window = sniffer_window
-        self.control_window = control_window
+        self.window = window
 
         # Set scapy sniff interface
         conf.iface = interface
 
         # Compile list of vendors
-        self.vendors = compile_vendors()
+        self.vendors = Utils.compile_vendors()
 
         self.ap_dict = dict()
         self.all_bssid = list()
@@ -60,7 +61,10 @@ class Blackout:
         self.target_client = None
 
         # Channel hopping thread
-        self.thread_channel_hop = Thread(target=Blackout.channel_hop, args=(conf.iface,))
+        self.thread_channel_hop = Thread(
+            target=Blackout.channel_hop,
+            args=(str(interface),))
+
         # Make the thread run in the background as a daemon
         self.thread_channel_hop.daemon = True
 
@@ -81,14 +85,14 @@ class Blackout:
                 'iface': conf.iface,
                 'store': 0})
 
-    def write_window(self, window, y, x, text, attr):
+    def write_window(self, y, x, text, attr):
         #self.stdscr.noutrefresh()
         if x and y:
-            window.addstr(y, x, text, attr)
+            self.window.addstr(y, x, text, attr)
         else:
-            window.addstr(text)
+            self.window.addstr(text)
 
-        window.noutrefresh()
+        self.window.noutrefresh()
         curses.doupdate()
 
     def run(self):
@@ -97,11 +101,11 @@ class Blackout:
         self.stdscr.clear()
 
         try:
-            start_mon(conf.iface, self.control_window)
+            self.utils.start_mon()
 
             # Start daemon thread
             self.thread_channel_hop.start()
-            self.write_window(self.sniffer_window, 1, 2, "[+] Sniffing for Access Points on all channels...", curses.A_BOLD)
+            self.write_window(2, 2, "[+] Sniffing for Access Points on all channels...", curses.A_BOLD)
 
             # Sniff for Wireless Access Points
             self.thread_sniff_ap.start()
@@ -110,8 +114,8 @@ class Blackout:
             self.thread_sniff_ap.join()
 
             # Output a very simple summary of findings
-            horizontal_rule(30, self.control_window)
-            self.write_windows(self.control_window, 1, 2, f"\nAccess Points discovered: {len(self.ap_dict)}\n\n")
+            self.utils.horizontal_rule(30)
+            self.write_window(1, 2, f"\nAccess Points discovered: {len(self.ap_dict)}\n\n")
 
             # Select an target AP
             self.target_ap = self.select_target_ap()
@@ -119,19 +123,19 @@ class Blackout:
             # Important to make it upper for comparisons in self.sniff_clients
             self.target_bssid = self.target_ap['bssid'].upper()
 
-            choice = choose_mode()
+            choice = self.utils.choose_mode()
 
             # Deuth ALL clients from AP
             if choice == '1':
-                Blackout.deauth(
+                self.deauth(
                     self.target_ap['bssid'],
                     str(self.target_ap['channel']),
                     BROADCAST_ADDR)
 
             # Deauth specific client from AP
             elif choice == '2':
-                horizontal_rule(30)
-                self.window_write(self.control_windows, f"Sniffing for clients of AP - {self.target_ap['bssid']}...")
+                self.utils.horizontal_rule(30)
+                self.window_write(f"Sniffing for clients of AP - {self.target_ap['bssid']}...")
                # print(f"{Colour.OKBLUE}Press Ctrl-c to select target{Colour.ENDC}\n")
 
                 self.proc_sniff_clients.start()
@@ -140,30 +144,23 @@ class Blackout:
                 self.list_clients()
                 self.select_target_client()
 
-                Blackout.deauth(
+                self.deauth(
                     self.target_ap['bssid'],
                     str(self.target_ap['channel']),
                     self.target_client)
 
         except Exception as e:
-            print(f"[!] Error: {e}")
+            self.write_window("blackout.run: {}".format(e))
+            Utils.log_error_to_file(e)
 
         except KeyboardInterrupt:
             pass
 
         finally:
             # Put network card back into managed mode
-            stop_mon(conf.iface)
+            self.utils.stop_mon()
 
-            # record the detected AP's
-            # with open("report.txt", "w") as f:
-            #     for k, v in _ap_dict.items():
-            #         f.write(f"{k} | {v[1]} | {v[2]}\n")
-
-            # END
-
-    @staticmethod
-    def deauth(bssid, channel, target):
+    def deauth(self, bssid, channel, target):
         """
         create deauth packets and send them to the target AP
         TODO:
@@ -195,7 +192,7 @@ class Blackout:
                 print("[*] Keyboard Interrupt\n")
             except Exception as e:
                 print(f"[!] Error: {e}")
-                stop_mon(conf.iface)
+                self.utils.stop_mon()
                 sys.exit(0)
 
             print("[*] Deauthentication Complete")
@@ -205,7 +202,7 @@ class Blackout:
             print(f"[!] Error while Deauthenticating: {e}")
 
     def select_target_client(self):
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
 
         target_id = int(input("\nEnter ID of the client you wish to target: "))
 
@@ -213,9 +210,9 @@ class Blackout:
         # [{'1': [client info, ]}, {'2', [client info, ... ]}, ... ]
         self.target_client = self.ap_clients[target_id - 1]
 
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
         print(f"\nTargeting Client: [{self.target_client}]\n")
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
 
     def sniff_clients(self, pkt):
 
@@ -249,26 +246,26 @@ class Blackout:
 
     def list_clients(self):
         print("\n")
-        horizontal_rule(20)
+        self.utils.horizontal_rule(20)
         for i, client in enumerate(self.ap_clients):
             # Find manufacturer
             name = self.get_vendor(client)
             print(f"{i + 1}) {client}\t{name}")
 
     def select_target_ap(self):
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
 
         target_id = int(input("\nEnter ID of the AP you wish to target: "))
 
         #  FORMAT: _ap_dict[count] = [bssid, ssid, channel, [clients, .. , ]]
         target_ap = self.ap_dict[target_id]
 
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
         print(f"\nSelected Access Point [{target_id}]\n")
         print(f"\tssid:\t\t{target_ap['ssid']:20}")
         print(f"\tbssid:\t\t{target_ap['bssid']:20}")
         print(f"\tchannel:\t\t{target_ap['channel']:20}\n")
-        horizontal_rule(30)
+        self.utils.horizontal_rule(30)
 
         return target_ap
 
@@ -286,7 +283,6 @@ class Blackout:
                 
                 # If the packet contains a BSSID we have not encountered
                 if pkt.addr3.upper() not in self.all_bssid:  # addr3 -> BSSID
-                    self.sniffer_window.addstr("\nFound a packet!\n")
                     self.refresh()
                     bssid = pkt.addr3.upper()  # add the bssid to our bssid list
                     self.all_bssid.append(bssid)
@@ -309,16 +305,15 @@ class Blackout:
                         # Output the catch
                         #self.sniffer_window.addstr(f"%2d)\t{Colour.OKBLUE}%-20s\t{Colour.OKGREEN}%-20s\t{Colour.ENDC}%2d\t\t%-20s\n" % (
                         #    count, ssid, bssid, channel, vendor))
-                        self.sniffer_window.erase()
-                        self.sniffer_window.addstr(1, 1, f"{count})\t{ssid}\t{bssid}\t{channel}\t\t{vendor}\n", curses.A_BOLD)
+                        self.window.addstr(1, 1, f"{count})\t{ssid}\t{bssid}\t{channel}\t\t{vendor}\n", curses.A_BOLD)
                         self.refresh()
 
                     except Exception as e:
                         if "ord" in f"{e}":  # TODO This may be to do with 5GHz channels cropping up?
                             pass
                         else:
-                            self.control_window.erase()
-                            self.control_window.addstr(f"[!] Sniffer Error: {e}")
+                            self.window.erase()
+                            self.window.addstr(f"[!] Sniffer Error: {e}")
                             self.refresh()
 
     def get_vendor(self, bssid):
@@ -331,7 +326,7 @@ class Blackout:
         return name
 
     @staticmethod
-    def channel_hop(self):
+    def channel_hop(iface):
         """
         Hop channels until interrupted
         """
@@ -350,7 +345,7 @@ class Blackout:
                 # Note: I don't seem to need to PIPE any outputs(stdout etc.) for this to work
 
                 #print("{} - {}".format(conf.iface, type(conf.iface)))
-                p = Popen(["iw", "dev", str(conf.iface), "set", "channel", str(channel)])
+                p = Popen(["iw", "dev", iface, "set", "channel", str(channel)])
                 try:
                     # effectively execute p
                     p.communicate()
@@ -358,13 +353,13 @@ class Blackout:
                 except KeyboardInterrupt:
                     break
                 except Exception as e:
-                    print(f"[!] {Colour.FAIL}Channel Hop Error: {e}{Colour.ENDC}")
+                    pass
 
     def signal_handler(self, sig, stack_frame):
         """
         This handles an interrupt signal, like
         Ctrl-C. Once detected, the program will sleep
-        So we can 'get our affairs in order'(read 'print summary')
+        So we can 'get our affairs in order'
         """
         sleep(1)
 
@@ -385,7 +380,11 @@ class Blackout:
             self.proc_sniff_clients.join()
         except Exception:
             pass
+
         finally:
+            self.utils.stop_mon()
+
+            # End curses
             curses.nocbreak()
             stdscr.keypad(False)
             curses.echo()
@@ -404,26 +403,19 @@ if __name__ == "__main__":
     curses.start_color()
 
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE) #color pair 1
-    highlightText = curses.color_pair(1) #color pair for highlighted menu option
-    normalText = curses.A_NORMAL #color pair for non-highlighted menu options
+    #highlightText = curses.color_pair(1) #color pair for highlighted menu option
+    #normalText = curses.A_NORMAL #color pair for non-highlighted menu options
 
     HEIGHT, WIDTH = stdscr.getmaxyx()
 
-    sniffer_window = curses.newwin(int(HEIGHT/2)-1, WIDTH-2, 1, 1)
-    sniffer_window.border('|', '|', '-', '-', '+', '+', '+', '+')
-    
-    control_window = curses.newwin(int(HEIGHT/2)-1, WIDTH-2, int(HEIGHT/2), 1)
-    control_window.border('|', '|', '-', '-', '+', '+', '+', '+')
-
-    sniffer_window.addstr(0, 2, "Sniffer Output", curses.A_NORMAL)
-    control_window.addstr(0, 2, "Control", curses.A_NORMAL)
+    window = curses.newwin(HEIGHT-2, WIDTH-2, 1, 1)
+    window.border('|', '|', '-', '-', '+', '+', '+', '+')
     
     stdscr.noutrefresh()
-    sniffer_window.noutrefresh()
-    control_window.noutrefresh()
+    window.noutrefresh()
     curses.doupdate()
 
-    blackout = Blackout("wlan0", stdscr, sniffer_window, control_window)
+    blackout = Blackout("wlan0", stdscr, window)
 
     # Set the signal handler
     signal.signal(signal.SIGINT, blackout.signal_handler)
@@ -432,12 +424,18 @@ if __name__ == "__main__":
         blackout.run()
 
     except Exception as e:
-        print(f"[!] Error running blackout.run(): {e}")
+        window.addstr(f"[!] Error running blackout.run(): {e}")
+        Utils.log_error_to_file(e)
+
 
     except KeyboardInterrupt:
         pass
 
     finally:
+
+        c = stdscr.getch()
+
+        # End curses
         curses.nocbreak()
         stdscr.keypad(False)
         curses.echo()
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 
-from curses import A_NORMAL
 import re
 import os
 import sys
@@ -8,147 +7,168 @@ import curses
 from time import sleep
 from subprocess import PIPE, Popen, check_output
 
-
-class Colour:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-def horizontal_rule(n, window):
-    window.addstr("-" * n)
-    window.addstr("\n")
-    window.noutrefresh()
-    curses.doupdate()
+# class Colour:
+#     HEADER = '\033[95m'
+#     OKBLUE = '\033[94m'
+#     OKGREEN = '\033[92m'
+#     WARNING = '\033[93m'
+#     FAIL = '\033[91m'
+#     ENDC = '\033[0m'
+#     BOLD = '\033[1m'
+#     UNDERLINE = '\033[4m'
 
 
-def self_mac(iface):
-    """
-    returns the mac address of your NIC or None
-    *not currently needed*
-    """
-    ifconfig = Popen(['ifconfig'], stdout=PIPE)
-    out = ifconfig.communicate()[0]
-    out = out[out.find(iface):]
-    pattern = r'(\w\w:?){6}'
-    try:
-        mac = re.search(pattern, out).group()
-    except AttributeError:
-        mac = "Unavailable"
-    return mac
+class Utils:
+
+    def __init__(self, iface, stdscr, window):
+        
+        self.iface = str(iface)
+        self.stdscr = stdscr
+        self.window = window
+        self.services = ['NetworkManager', 'wpa_supplicant']
 
 
-def get_mac(iface):
-    """
-    returns the mac address of your NIC or None
-    *not currently needed*
-    """
-    # call ifconfig and parse the output to find our iface
-    ifconfig = Popen(['ifconfig'], stdout=PIPE)
-    out = ifconfig.communicate()[0]
-    out = out[out.find(iface):]
-
-    # regex pattern to match a MAC address
-    pattern = r'ether ((\w\w:?){6})'
-    try:
-        mac = re.search(pattern, out).groups()[0]
-    except AttributeError:
-        mac = "Unavailable"
-
-    return mac
-
-
-def start_mon(iface, window):
-    """
-    Put wireless network interface into monitor mode
-    """
-
-    try:
-        window.addstr(1, 2, f"[+] Putting {iface} into MONITOR mode", curses.A_NORMAL)
-        window.noutrefresh()
+    def horizontal_rule(self, n):
+        self.window.addstr("-" * n)
+        self.window.addstr("\n")
+        self.window.noutrefresh()
         curses.doupdate()
 
-        # Need to kill any processes that my change channels or put interface back into MANAGED mode
 
-        # Stop NetworkManager service
-        Popen(['systemctl', 'stop', 'NetworkManager']).communicate()
-
-        # Kill wpa_supplicant process if running
-        out = check_output(['ps', '-ef'])
-        pid = [proc for proc in out.split(b'\n') if b'wpa_supplicant' in proc][0].split()[1].decode()
-        if pid.isdigit():
-            Popen(['kill', pid]).communicate()
-
-        # Ex: ip link set wlan0 down
-        iface_down = Popen(['ip', 'link', 'set', str(iface), 'down'])
-        sleep(2)
-        # Ex: iw wlan0 set monitor control
-        set_monitor_mode = Popen(['iw', str(iface), 'set', 'monitor', 'control'])
-        sleep(2)
-        # Ex: ip link set wlan0 up
-        iface_up = Popen(['ip', 'link', 'set', str(iface), 'up'])
-        sleep(2)
-        
-        iface_down.communicate()
-        set_monitor_mode.communicate()
-        iface_up.communicate()
-
-    except Exception as e:
-        #print(f"{Colour.WARNING}FAILED")
-        print(f"[!] Error putting {iface} into MONITOR mode.{Colour.ENDC}")
-        print(f"{Colour.FAIL}[!] {e}{Colour.ENDC}")
-        print("[-] Exiting...")
-        sys.exit(0)
+    def self_mac(self):
+        """
+        returns the mac address of your NIC or None
+        *not currently needed*
+        """
+        ifconfig = Popen(['ifconfig'], stdout=PIPE)
+        out = ifconfig.communicate()[0]
+        out = out[out.find(self.iface):]
+        pattern = r'(\w\w:?){6}'
+        try:
+            mac = re.search(pattern, out).group()
+        except AttributeError:
+            mac = "Unavailable"
+        return mac
 
 
-def stop_mon(iface):
-    """
-    Put wireless network interface back into managed mode
-    """
-    try:
-        print(f"Putting {iface} back into MANAGED mode: ", end="")
-        os.system(f"ifconfig {iface} down")
-        os.system(f"iw {iface} set type managed")
-        os.system(f"ifconfig {iface} up")
+    def get_mac(self):
+        """
+        returns the mac address of your NIC or None
+        *not currently needed*
+        """
+        # call ifconfig and parse the output to find our iface
+        ifconfig = Popen(['ifconfig'], stdout=PIPE)
+        out = ifconfig.communicate()[0]
+        out = out[out.find(self.iface):]
 
-        # reconnect to any wifi network
-        os.system("service NetworkManager restart")
-        print(f"{Colour.OKGREEN}SUCCESS{Colour.ENDC}")
+        # regex pattern to match a MAC address
+        pattern = r'ether ((\w\w:?){6})'
+        try:
+            mac = re.search(pattern, out).groups()[0]
+        except AttributeError:
+            mac = "Unavailable"
 
-    except Exception as e:
-        print(f"{Colour.WARNING}FAILED")
-        print(f"[!] Error putting {iface} into MANAGED mode.{Colour.ENDC}")
-        print(f"{Colour.FAIL}[!] Error: {e}{Colour.ENDC}")
-        sys.exit(0)
-
-
-def compile_vendors():
-    with open('vendors.pickle', 'rb') as f:
-        # FORMAT -> {"XX:YY:ZZ": MANUFACTURER, "AA:BB:CC": MANUFACTURER, ... etc }
-        vendors = pickle.load(f)
-
-    return vendors
+        return mac
 
 
-def print_headers(self):
-    # Print column headings
-    print(f"\n{Colour.HEADER}::ID\t%-20s\t%-20s\t::CHANNEL\t\t%-20s\n{Colour.ENDC}" % (
-        "::SSID", "::BSSID", "::VENDOR"))
-    horizontal_rule(100)
+    def service_is_active(self, service):
+        # Will exit with status zero if service is active, non-zero otherwise
+        return check_output(['systemctl', 'is-active', '--quiet', service]) == 0
 
 
-def choose_mode():
-    print('\n')
-    print("1) Deauthenticate ALL clients from AP")
-    print("2) Deauthenticate specific client from AP (sniff clients)\n")
-    choice = input("[*] Enter choice: ")
+    def service_control(self, action, service):
+        Popen(['systemctl', action, service]).communicate()
+        self.window.addstr(f"[+] {action}: {service}\n", curses.A_NORMAL)
 
-    while choice not in ('1', '2'):
-        print("[!] Please enter a valid choice\n")
 
-    return choice
+    def start_mon(self):
+        """
+        Put wireless network interface into monitor mode
+        """
+
+        try:
+            self.window.addstr(1, 2, f"[+] Putting {self.iface} into MONITOR mode\n", curses.A_NORMAL)
+            self.window.noutrefresh()
+            curses.doupdate()
+
+            # Need to kill any processes that my change channels or put interface back into MANAGED mode
+            for service in self.services:
+                if self.service_is_active(service):
+                    self.debug(f"{service} is active")
+                    self.service_control('stop', service)
+
+            # Ex: ip link set wlan0 down
+            iface_down = Popen(['ip', 'link', 'set', self.iface, 'down'])
+            sleep(2)
+            # Ex: iw wlan0 set monitor control
+            set_monitor_mode = Popen(['iw', self.iface, 'set', 'monitor', 'control'])
+            sleep(2)
+            # Ex: ip link set wlan0 up
+            iface_up = Popen(['ip', 'link', 'set', self.iface, 'up'])
+            sleep(2)
+            
+            iface_down.communicate()
+            set_monitor_mode.communicate()
+            iface_up.communicate()
+
+        except Exception as e:
+            self.window.addstr(1, 50, f"[+] Error start_mon: {e}", curses.A_NORMAL)
+            Utils.log_error_to_file(e)
+
+
+    def stop_mon(self):
+        """
+        Put wireless network interface back into managed mode
+        """
+        try:
+            self.window.addstr(1, 2, f"[+] Putting {self.iface} back into MANAGED mode", curses.A_NORMAL)
+            self.window.noutrefresh()
+            curses.doupdate()
+
+            os.system(f"ifconfig {self.iface} down")
+            os.system(f"iw {self.iface} set type managed")
+            os.system(f"ifconfig {self.iface} up")
+
+            # Restart any stopped services
+            for service in self.services:
+                self.service_control('start', service)
+
+        except Exception as e:
+            self.debug(f"Stop_mon: {e}")
+
+
+    def print_headers(self):
+        # Print column headings
+        self.window.addstr(1, 2, "\n::ID\t%-20s\t%-20s\t::CHANNEL\t\t%-20s\n" % ("::SSID", "::BSSID", "::VENDOR"), curses.A_NORMAL)
+        self.window.noutrefresh()
+        curses.doupdate()
+        self.horizontal_rule(100)
+
+ 
+    def choose_mode(self):
+        print('\n')
+        print("1) Deauthenticate ALL clients from AP")
+        print("2) Deauthenticate specific client from AP (sniff clients)\n")
+        choice = input("[*] Enter choice: ")
+
+        while choice not in ('1', '2'):
+            print("[!] Please enter a valid choice\n")
+
+        return choice
+
+
+    def debug(self, msg):
+        self.window.addstr(10, 2, f"{msg}", curses.A_NORMAL)
+
+    @staticmethod
+    def compile_vendors():
+        with open('vendors.pickle', 'rb') as f:
+            # FORMAT -> {"XX:YY:ZZ": MANUFACTURER, "AA:BB:CC": MANUFACTURER, ... etc }
+            vendors = pickle.load(f)
+        return vendors
+
+    @staticmethod
+    def log_error_to_file(error):
+        with open('log', 'a') as f:
+            f.write(str(error) + '\n')
+
